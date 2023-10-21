ff9486be834285b529f04ba2ad94e82fa622799b
ranton <ranton@ttu.ee>
2023-10-21 23:07:26 +0300

    Add basic version

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from srt_translator import SrtTranslator
-import os
 
-def  main():
+EST = "et"
+ENGLISH = "en"
+UKRAINIAN = "uk"
+
+# Add the output and input folder so the translator can find the place
+# where the .srt files are and where to put the translaited versions.
+INPUT_FOLDER_PATH = ""
+OUTPUT_FOLDER_PATH = ""
+
+def main():
     srtt = SrtTranslator()
-    
-if __name__ == "__main__":
+    srtt.translateFolder(INPUT_FOLDER_PATH, OUTPUT_FOLDER_PATH)
+
+if __name__ == '__main__':
     main()
